814a80f80d069d090505b9a9f7d2d325dabb4f84
Shar <81372629+shar27@users.noreply.github.com>
2024-10-02 11:31:12 +0200

    landowner page

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Button() {
+function Button({text, color, bgColor}) {
   return (
-    <button className="text-white bg-black border 
-    border-white p-3 w-48 text-xl font-bold">Get Started</button>
+    // <button className="text-white bg-black border 
+    // border-white p-3 w-48 text-xl font-bold">{text}</button>
+
+    <button className={` border border-white p-3 w-48 text-xl font-bold ${color} ${bgColor} `}>
+      {text}
+    </button>
   )
 }
 
