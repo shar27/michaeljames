7f6df77ff7691a4ca083062ac935c2b42ec1b6a6
Shar <81372629+shar27@users.noreply.github.com>
2024-10-05 18:32:29 +0200

    links, spacing

--- src/app/components/Deals.jsx
+++ src/app/components/Deals.jsx
@@ -33,9 +33,13 @@ const Deals = () => {
   }
   //   console.log(landDeals);
   console.log(propDeals);
+  console.log(landDeals);
+  
 
   return (
     <div>
+      <h2 className="bg-white text-black text-center text-3xl">Current Land deals</h2>
+      <h3 className="bg-white text-black text-center text-3xl">Can't find what you're looking for? Sign up for <Link className="text-black font-bold underline" href="BespokeSourcing">Bespoke-sourcing</Link> </h3>
       {/* Display land deals */}
       {landDeals.length === 0 ? (
         <div>No land deals available</div>
@@ -55,7 +59,7 @@ const Deals = () => {
               </h2>
 
               <p>{ldeal.fields.propertyType}</p>
-              <p>Purchase Price:£ {ldeal.fields.rent}</p>
+              <p>Purchase Price:£ {ldeal.fields.purchasePrice}</p>
               <Image
                 src={`https:${ldeal.fields.featured.fields.file.url}`}
                 alt={ldeal.fields.featured.fields.title || "Land Deal Image"}
@@ -75,9 +79,12 @@ const Deals = () => {
         </div>
       )}
 
+      <h2 className="bg-white text-black text-center text-3xl">Current Property Deals</h2>
+      <h3 className="bg-white text-black text-center text-3xl">Can't find what you're looking for? Sign up for <Link className="text-black font-bold underline" href="BespokeSourcing">Bespoke-sourcing</Link> </h3>
+
       {/* Display property deals */}
       {propDeals.length === 0 ? (
-        <div>No property deals available</div>
+        <div>Can't find what you're looking for? Sign up for  <Link href="BespokeSourcing">Bespoke-sourcing</Link> </div>
       ) : (
         <div className="grid grid-cols-1 lg:grid-cols-3 p-5 gap-20 bg-white text-black">
           {propDeals.map((deal) => (
@@ -86,7 +93,7 @@ const Deals = () => {
                 {deal.fields.heading}
               </h2>
               <p>{deal.fields.propertyType}</p>
-              <p>Rent: ${deal.fields.rent}</p>
+              <p>Rent: £{deal.fields.rent}</p>
               <Image
                 src={`https:${deal.fields.featured.fields.file.url}`}
                 alt={
@@ -98,9 +105,9 @@ const Deals = () => {
                 layout="responsive"
               />
               <div className="mt-auto p-5">
-                <Link href={`/LiveDeals/${deal.fields.slug}`}>
+              <Link href={`https://calendly.com/michael-u4i/`}>
                   <button className="border border-black text-black p-2 rounded-sm w-full h-12">
-                    Read more
+                   Learn More
                   </button>
                 </Link>
               </div>
