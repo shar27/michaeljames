b7b86d052f68ac87995363782bc4236f40f4cd2f
Shar <81372629+shar27@users.noreply.github.com>
2024-10-06 17:46:04 +0200

    update refund policy

--- src/app/RefundPolicy/page.js
+++ src/app/RefundPolicy/page.js
@@ -1,86 +1,184 @@
-import React from 'react';
-
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 function RefundPolicy() {
   return (
+    <>
+    <NavBar/>
     <div className="p-4">
-      <h1>Refund Policy</h1>
-      <p>
+      <h1 className="text-5xl text-center p-2">Refund Policy</h1>
+      <p className="text-xl p-2">
         Michael James Investment Group Ltd (Trading as: Michael James Property)
       </p>
-      <p>Refund Policy Last Updated: 20/09/2023</p>
+      <p className="text-xl p-2">Refund Policy Last Updated: 20/09/2023</p>
 
-      <h2>PURCHASING A DEAL WITH US (without All Inclusive)</h2>
-      <p>An alternative deal will be given:</p>
-      <ul>
-        <li>If the Vendor pulls out not due to actions of the Investor.</li>
-        <li>
-          If the refurb quote is significantly larger than initially stated, provided no extras have been included by the Investor; if the Investor instructs searches and/or surveys, in-depth information that Michael James Property would not have been aware of may come to light. (This can potentially affect the refurb cost, valuation, and/or sale price. This is a rare occasion, but we must make you aware of these potential occurrences.) By buying a deal from us, the Investor is agreeing to not hold Michael James Property accountable and these revelations from searches and surveys may not entitle the Investor to an alternative deal [refund].
+      <h2 className="text-4xl mt-2">
+        PURCHASING A DEAL WITH US (without All Inclusive)
+      </h2>
+      <p className="text-xl p-2">An alternative deal will be given:</p>
+      <ul className="p-3">
+        <li className="list-disc p-4">
+          If the Vendor pulls out not due to actions of the Investor.
         </li>
-        <li>If the property is genuinely down-valued and there is no course of appeal action.</li>
-        <li>If professional negligence by an appointed member of our power team occurs.</li>
-        <li>If the rental valuation for the property is considerably less than was initially sold.</li>
-        <li>If the purchase price on the original listing is wrong.</li>
-        <li>
-          If the sale continues for 9 months or more from the date the memorandum of sale is sent out solely due to the vendor deliberately slowing progress.
+        <li className="list-disc p-4">
+          If the refurb quote is significantly larger than initially stated,
+          provided no extras have been included by the Investor; if the Investor
+          instructs searches and/or surveys, in-depth information that Michael
+          James Property would not have been aware of may come to light. (This
+          can potentially affect the refurb cost, valuation, and/or sale price.
+          This is a rare occasion, but we must make you aware of these potential
+          occurrences.) By buying a deal from us, the Investor is agreeing to
+          not hold Michael James Property accountable and these revelations from
+          searches and surveys may not entitle the Investor to an alternative
+          deal [refund].
         </li>
-        <li>
-          If there are issues with the Legals of the property; terms within the lease prevent desired course of action, issues with title, property not fit for use due to planning where there are no alternative courses of action to resolve the matter.
+        <li className="list-disc p-4">
+          If the property is genuinely down-valued and there is no course of
+          appeal action.
         </li>
-        <li>If the tenant type differs from that which was originally sold whereby the deal does not stack as well as initially stated.</li>
-      </ul>
-
-      <p>A refund will not be given:</p>
-      <ul>
-        <li>
-          If you do not supply Michael James Property with the following information within 5 working days, £1000.00 will be deducted from your refund amount:
-          <ul>
-            <li>How you are purchasing the property: mortgage / cash / bridging finance</li>
-            <li>If you are buying in a company name or under your name?</li>
-            <li>Certified ID (in pdf format)</li>
-            <li>Certified proof of address (in pdf format)</li>
-            <li>3 months bank statement as proof of funds (in pdf format)</li>
-            <li>If you're buying under a company name, 3 months of company accounts in pdf format as proof of funds</li>
-          </ul>
+        <li className="list-disc p-4">
+          If professional negligence by an appointed member of our power team
+          occurs.
         </li>
-        <li>If the Investor does not have enough cash to complete.</li>
-        <li>If the Investor is happy to still proceed with the property purchase, even if changes to the property opportunity presented change.</li>
-        <li>If the Investor refuses to proceed with a solution to a problem that has arisen throughout the process and the end result would be a deal that still stacks as well as or better than as was initially sold.</li>
-        <li>If the property market changes since the sale was agreed.</li>
-        <li>
-          If by acts, events, omissions, or accidents beyond the company’s reasonable control, including, without limitation, failure of a utility service or transport or telecommunications network, act of God, war, riot, civil commotion, malicious damage, pandemic, epidemic compliance with any law or governmental order, rule, regulation, or direction, accident, fire, flood, or storm, provided that the Investor is notified of such an event and its expected duration. (This includes COVID-19).
+        <li className="list-disc p-4">
+          If the rental valuation for the property is considerably less than was
+          initially sold.
         </li>
-        <li>If the Investor makes a mistake.</li>
-        <li>If the Investor changes their mind about how they wish to invest in property.</li>
-        <li>If planning fails unless stipulated otherwise.</li>
-        <li>If the Investor goes against advice given by The Property Network which results in the deal falling through.</li>
-        <li>If contracts have been exchanged in conveyancing.</li>
-        <li>For any reasonable unforeseen circumstance that could not have realistically or reasonably been predicted or prevented ahead of discovery by The Property Network.</li>
-        <li>For any personal circumstance for the Investor that means they are not able to proceed with the deal, such as but not limited to:
-          <ul>
-            <li>Bereavement</li>
-            <li>Loss of earnings</li>
-            <li>Loss of capital</li>
-            <li>Time constraints</li>
-          </ul>
+        <li className="list-disc p-4">
+          If the purchase price on the original listing is wrong.
+        </li>
+        <li className="list-disc p-4">
+          If the sale continues for 9 months or more from the date the
+          memorandum of sale is sent out solely due to the vendor deliberately
+          slowing progress.
+        </li>
+        <li className="list-disc p-4">
+          If there are issues with the Legals of the property; terms within the
+          lease prevent desired course of action, issues with title, property
+          not fit for use due to planning where there are no alternative courses
+          of action to resolve the matter.
+        </li>
+        <li className="list-disc p-4">
+          If the tenant type differs from that which was originally sold whereby
+          the deal does not stack as well as initially stated.
         </li>
       </ul>
 
-      <h2>BUYING A DEAL WITH US (with All Inclusive)</h2>
-      <p>
-        All Inclusive is non-refundable after 14 days. A refund may be given but solely at the discretion of the company and is not normal business practice to issue a refund after the 14-day cooling off period.
+      <p className="text-xl p-2">A refund will not be given:</p>
+      <ul className="p-3">
+        
+          <ul className="p-3">
+          <li className="font-bold p-4">
+          If you do not supply Michael James Property with the following
+          information within 5 working days, £1000.00 will be deducted from your
+          refund amount:</li>
+            <li className="list-disc p-4">
+              How you are purchasing the property: mortgage / cash / bridging
+              finance
+            </li>
+            <li className="list-disc p-4">
+              If you are buying in a company name or under your name?
+            </li>
+            <li className="list-disc p-4">Certified ID (in pdf format)</li>
+            <li className="list-disc p-4">
+              Certified proof of address (in pdf format)
+            </li>
+            <li className="list-disc p-4">
+              3 months bank statement as proof of funds (in pdf format)
+            </li>
+            <li className="list-disc p-4">
+              If you're buying under a company name, 3 months of company
+              accounts in pdf format as proof of funds
+            </li>
+          </ul>
+        
+        <li className="list-disc p-4">
+          If the Investor does not have enough cash to complete.
+        </li>
+        <li className="list-disc p-4">
+          If the Investor is happy to still proceed with the property purchase,
+          even if changes to the property opportunity presented change.
+        </li>
+        <li className="list-disc p-4">
+          If the Investor refuses to proceed with a solution to a problem that
+          has arisen throughout the process and the end result would be a deal
+          that still stacks as well as or better than as was initially sold.
+        </li>
+        <li className="list-disc p-4">
+          If the property market changes since the sale was agreed.
+        </li>
+        <li className="list-disc p-4">
+          If by acts, events, omissions, or accidents beyond the company’s
+          reasonable control, including, without limitation, failure of a
+          utility service or transport or telecommunications network, act of
+          God, war, riot, civil commotion, malicious damage, pandemic, epidemic
+          compliance with any law or governmental order, rule, regulation, or
+          direction, accident, fire, flood, or storm, provided that the Investor
+          is notified of such an event and its expected duration. (This includes
+          COVID-19).
+        </li>
+        <li className="list-disc p-4">If the Investor makes a mistake.</li>
+        <li className="list-disc p-4">
+          If the Investor changes their mind about how they wish to invest in
+          property.
+        </li>
+        <li className="list-disc p-4">
+          If planning fails unless stipulated otherwise.
+        </li>
+        <li className="list-disc p-4">
+          If the Investor goes against advice given by The Property Network
+          which results in the deal falling through.
+        </li>
+        <li className="list-disc p-4">
+          If contracts have been exchanged in conveyancing.
+        </li>
+        <li className="list-disc p-4">
+          For any reasonable unforeseen circumstance that could not have
+          realistically or reasonably been predicted or prevented ahead of
+          discovery by The Property Network.
+        </li>
+        <li className="list-disc p-4">
+          For any personal circumstance for the Investor that means they are not
+          able to proceed with the deal, such as but not limited to:
+          <ul className="p-3">
+            <li className="list-disc p-4">Bereavement</li>
+            <li className="list-disc p-4">Loss of earnings</li>
+            <li className="list-disc p-4">Loss of capital</li>
+            <li className="list-disc p-4">Time constraints</li>
+          </ul>
+        </li>
+      </ul>
+
+      <h2 className="text-4xl mt-2">
+        BUYING A DEAL WITH US (with All Inclusive)
+      </h2>
+      <p className="text-xl p-2">
+        All Inclusive is non-refundable after 14 days. A refund may be given but
+        solely at the discretion of the company and is not normal business
+        practice to issue a refund after the 14-day cooling off period.
       </p>
-      <p>
-        If you are within your 14-day cooling off period and do not supply your case handler with the following information within 5 working days as per your contract, £1000.00 will be deducted from your refund amount:
+      <p className="text-xl p-2">
+        If you are within your 14-day cooling off period and do not supply your
+        case handler with the following information within 5 working days as per
+        your contract, £1000.00 will be deducted from your refund amount:
       </p>
-      <ul>
-        <li>How you are purchasing the property: mortgage / cash / bridging finance</li>
-        <li>If you are buying in a company name or under your name?</li>
-        <li>Certified ID (in pdf format)</li>
-        <li>Certified proof of address (in pdf format)</li>
-        <li>3 months bank statement as proof of funds (in pdf format)</li>
-        <li>If you're buying under a company name, 3 months of company accounts in pdf format as proof of funds</li>
+      <ul className="p-3">
+        <li className="list-disc p-4">
+          How you are purchasing the property: mortgage / cash / bridging
+          finance
+        </li>
+        <li className="list-disc p-4">If you are buying in a company name or under your name?</li>
+        <li className="list-disc p-4">Certified ID (in pdf format)</li>
+        <li className="list-disc p-4">Certified proof of address (in pdf format)</li>
+        <li className="list-disc p-4">3 months bank statement as proof of funds (in pdf format)</li>
+        <li className="list-disc p-4">
+          If you're buying under a company name, 3 months of company accounts in
+          pdf format as proof of funds
+        </li>
       </ul>
     </div>
+    <Footer/>
+    </>
   );
 }
 
