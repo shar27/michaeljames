2880e9c97cbb0cf406a52788d459b55c4e58da93
Shar <81372629+shar27@users.noreply.github.com>
2024-10-08 13:09:12 +0200

    added services section with svg

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -26,56 +26,74 @@ const Services = () => {
   
 
   return (
-    <div>
-      <section className="p-4 bg-white text-black">
-      <h2 className="bg-white text-black text-center text-5xl">
-        Services I Offer
-      </h2>
-      <h3 className="bg-white text-black text-center text-3xl">
+    <div className="bg-white min-h-screen mx-0 flex flex-col justify-center items-center">
+    <section className="p-4 text-black text-center">
+      <h2 className="text-black text-5xl">Services I Offer</h2>
+      <h3 className="text-black text-3xl">
         Can't find what you're looking for? Sign up for{" "}
-        <Link className="text-black font-bold underline" href="BespokeSourcing">
+        <Link className="text-black font-bold underline text-3xl" href="BespokeSourcing">
           Bespoke-sourcing
-        </Link>{" "}
+        </Link>
       </h3>
-      </section>
-      {/* Display land deals */}
-      {serviceSection.length === 0 ? (
-        <div></div>
-      ) : (
-        <div className="grid grid-cols-1 lg:grid-cols-3 p-5 gap-20 bg-white text-black">
-          {serviceSection.map((ldeal) => (
-            <div className="shadow-lg p-10 flex flex-col" key={ldeal.sys.id}>
-              <h2
-                className={"text-xl lg:text-3xl font-bold p-2 leading-10 uppercase text-center" }
-              >
-                {ldeal.fields.heading}
-              </h2>
-
-              
-              <div>{documentToReactComponents(ldeal.fields.description)}</div>
-              {/* <Image
-                src={`https:${ldeal.fields.featured.fields.file.url}`}
-                alt={ldeal.fields.featured.fields.title || "Our Services"}
-                width={500}
-                height={300}
-                layout="responsive"
-              /> */}
-              <div className="mt-auto p-5">
-                <Link href={ldeal.fields.link}>
-                  <button className="border border-black text-black p-2 rounded-sm w-full h-12">
-                    Learn More
-                  </button>
-                </Link>
-              </div>
+    </section>
+  
+    {/* Display land deals */}
+    {serviceSection.length === 0 ? (
+      <div></div>
+    ) : (
+      <div className="grid grid-cols-1 lg:grid-cols-3 p-5 gap-20 text-black w-full max-w-6xl">
+        {serviceSection
+        .sort((a,b) => a.fields.order - b.fields.order)
+        .map((ldeal) => (
+          <div
+            className="shadow-lg p-10 flex flex-col rounded-lg h-auto"
+            key={ldeal.sys.id}
+          >
+           
+          <img
+            src={`https:${ldeal.fields?.featured?.fields?.file.url}`} 
+            alt={ldeal.fields?.heading}
+            className=""
+          />
+        
+            <h2
+              className={
+                "text-xl lg:text-3xl font-bold p-2 leading-10 uppercase text-center"
+              }
+            >
+              {ldeal.fields.heading}
+            </h2>
+  
+            <p className="text-wrap text-center">
+              {documentToReactComponents(ldeal.fields.description)}
+            </p>
+            <div className="mt-auto p-5">
+              <Link href={ldeal.fields.link}>
+                <button className="border border-black text-black p-2 rounded-sm w-full h-12">
+                  Learn More
+                </button>
+              </Link>
             </div>
-          ))}
-        </div>
-      )}
-      <section className="text-center p-2 bg-white">
-      <p className="text-center bg-white text-black">If you believe we can add value to each other in a way that’s not covered above, don’t hesitate to reach out. I’m always open to new ideas.</p>
-      <Button text={'Speak to us'} padding={'p-3'} bgColor={'bg-black'} color={'text-white'}/>
+          </div>
+        ))}
+      </div>
+    )}
+  
+    <section className="text-center p-4">
+      <p className="text-center text-black">
+        If you believe we can add value to each other in a way that’s not covered above, don’t hesitate to reach out. I’m always open to new ideas.
+      </p>
+      <section className="mt-10">
+      <Button
+        text={"Speak to us"}
+        padding={"p-3"}
+        bgColor={"bg-black"}
+        color={"text-white"}
+      />
       </section>
-    </div>
+    </section>
+  </div>
+  
   );
 };
 
